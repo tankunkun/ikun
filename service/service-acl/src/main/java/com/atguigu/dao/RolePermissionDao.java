e7981ac375d2373d05314e249ac66cebac29184d
tankun1 <11718591+tankun1@user.noreply.gitee.com>
2023-06-05 05:12:42 +0800

    提交到本地

--- service/service-acl/src/main/java/com/atguigu/dao/RolePermissionDao.java
+++ service/service-acl/src/main/java/com/atguigu/dao/RolePermissionDao.java
@@ -2,8 +2,8 @@ package com.atguigu.dao;
 
 import com.atguigu.base.BaseDao;
 import com.atguigu.entity.RolePermission;
-
 import java.util.List;
+
 
 public interface RolePermissionDao extends BaseDao<RolePermission> {
     //根据角色id查询许可权限id集合
